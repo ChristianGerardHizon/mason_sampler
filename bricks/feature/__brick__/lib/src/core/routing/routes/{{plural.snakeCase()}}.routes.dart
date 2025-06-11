659f0eec0a9447882f294b3e53b9a28aafc601b5
Christian Hizon <christiangerardhizon@gmail.com>
2025-06-11 19:29:59 +0800

    cleanup

--- bricks/feature/__brick__/lib/src/core/routing/routes/{{plural.snakeCase()}}.routes.dart
+++ bricks/feature/__brick__/lib/src/core/routing/routes/{{plural.snakeCase()}}.routes.dart
@@ -5,48 +5,17 @@ class {{plural.pascalCase()}}BranchData extends StatefulShellBranchData {
 
   static const shellBranch = TypedStatefulShellBranch<{{plural.pascalCase()}}BranchData>(
     routes: <TypeRouteData>[
-      TypedGoRoute<{{plural.pascalCase()}}PageRoute>(path: {{plural.pascalCase()}}PageRoute.path),
-      TypedGoRoute<{{singular.pascalCase()}}FormPageRoute>(path: {{singular.pascalCase()}}FormPageRoute.path),
-      TypedGoRoute<{{singular.pascalCase()}}PageRoute>(path: {{singular.pascalCase()}}PageRoute.path),
+
+      /// Add Branch Routes Here
     ],
   );
 
 }
 
-
-///
-/// List
-///
-{{^isShellRoute}}@TypedGoRoute<{{plural.pascalCase()}}PageRoute>(path: {{plural.pascalCase()}}PageRoute.path){{/isShellRoute}}
-class {{plural.pascalCase()}}PageRoute extends GoRouteData {
-  const {{plural.pascalCase()}}PageRoute();
-  static const path = '/{{plural.lowerCase()}}/list';
-
-  @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return const {{plural.pascalCase()}}Page();
-  }
-}
-
-///
-/// Form
-///
-{{^isShellRoute}}@TypedGoRoute<{{singular.pascalCase()}}FormPageRoute>(path: {{singular.pascalCase()}}FormPageRoute.path){{/isShellRoute}}
-class {{singular.pascalCase()}}FormPageRoute extends GoRouteData {
-  const {{singular.pascalCase()}}FormPageRoute({this.id});
-  static const path = '/{{plural.lowerCase()}}/form';
-
-  final String? id;
-
-  @override
-  Widget build(BuildContext context, GoRouterState state) {
-    return {{singular.pascalCase()}}FormPage(id: id);
-  }
-}
-
 ///
 /// Details
 /// 
+/*
 {{^isShellRoute}}@TypedGoRoute<{{singular.pascalCase()}}PageRoute>(path: {{singular.pascalCase()}}PageRoute.path){{/isShellRoute}}
 class {{singular.pascalCase()}}PageRoute extends GoRouteData {
   const {{singular.pascalCase()}}PageRoute(this.id);
@@ -59,3 +28,4 @@ class {{singular.pascalCase()}}PageRoute extends GoRouteData {
     return {{singular.pascalCase()}}Page(id);
   }
 }
+*/
